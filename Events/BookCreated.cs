ea1afc3d1bc23a557114b2f9b2c246e20fd71309
Codrin Paun <Codrin.Paun@fnz.com>
2024-10-09 08:25:33 +0200

    - Added events

---
+++ Events/BookCreated.cs
@@ -0,0 +1,14 @@
+﻿using BookProject.Models;
+
+namespace BookProject.Events
+{
+    public class BookCreated : Event
+    {
+        public Guid BookId { get; init; }
+        public string Title { get; init; }
+        public string Description { get; init; }
+        public List<Author> Authors { get; init; }
+        public DateTime? PublishedDate { get; init; }
+        public override Guid Id => BookId;
+    }
+}
---
+++ Events/BookUpdated.cs
@@ -0,0 +1,42 @@
+﻿using BookProject.Models;
+
+namespace BookProject.Events
+{
+    public class BookUpdated : Event
+    {
+        public Guid BookId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public List<Author> Authors { get; set; }
+        public DateTime? PublishedDate { get; set; }
+        public override Guid Id => BookId;
+
+        public string GetUpdatedValues()
+        {
+            var outputTemplate = "The {0} property was changed to: {1} ";
+            var result = string.Empty;
+
+            if (Title != null)
+            {
+                result += string.Format(outputTemplate, nameof(Title), Title);
+            }
+
+            if (Description != null)
+            {
+                result += string.Format(outputTemplate, nameof(Description), Description);
+            }
+
+            if (Authors != null)
+            {
+                result += string.Format(outputTemplate, nameof(Authors), string.Join(", ", Authors.Select(x => x.Name)));
+            }
+
+            if (PublishedDate != null)
+            {
+                result += string.Format(outputTemplate, nameof(PublishedDate), PublishedDate);
+            }
+
+            return result;
+        }
+    }
+}
